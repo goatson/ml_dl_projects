cf11bc567121dbfbb4193aa11c914628c083f580
goatson <goatson@email>
2020-01-20 20:52:55 +0900

    su

--- Phython/Exercise/day9-flask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route("/hello")
-def hello():                           
-    return "<h1>JaeSung Fighting</h1>"
-
-if __name__ == "__main__":              
-    app.run(host="127.0.0.1", port="8080")
